93712ab4e017d79f1e1bc8fd69c1e2a8a1d5d689
Vlad Stepanov <vlad.0.stepanov@yandex.ru>
2025-03-05 15:08:52 +0200

    Edit styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
   const [count, setCount] = useState(0)
 
+  function plusClick() {
+    setCount(count + 1)
+  }
+
+  function minusClick() {
+    setCount(count - 1)
+  }
+
+  function resetCount() {
+    setCount(0)
+  }
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='app'>
+      <h1>{count}</h1>
+      <div className='buttons'>
+        <button onClick={minusClick}> Deduct One </button>
+        <button onClick={resetCount}> Reset </button>
+        <button onClick={plusClick}> Add One </button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+
+    </div>
   )
 }
 
